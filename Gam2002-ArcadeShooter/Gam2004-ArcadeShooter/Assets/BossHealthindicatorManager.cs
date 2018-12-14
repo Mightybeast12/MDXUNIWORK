c6d611c30d160d32a89b3021d93453324059a19e
Unknown <wmhonca@gmail.com>
2018-12-14 12:35:47 +0000

    12-14

---
+++ Gam2002-ArcadeShooter/Gam2004-ArcadeShooter/Assets/BossHealthindicatorManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealthindicatorManager : MonoBehaviour {
+    public GameObject BossManager;
+    public SpawnEnemiesatrandom SER;
+    public float BossmaxHealth;
+    public Text Health;
+    public float amountofhealth;
+    public Slider Healthindicator;
+	// Use this for initialization
+	void Start () {
+       
+        Healthindicator.maxValue = BossmaxHealth;
+        Healthindicator.value = BossmaxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SER = GameObject.FindGameObjectWithTag("SER").GetComponent<SpawnEnemiesatrandom>();
+        amountofhealth =  (Healthindicator.value);
+        Health.text = BossmaxHealth.ToString()+ "/" +  amountofhealth.ToString();
+
+        if (amountofhealth <= 0)
+        {
+            Destroy(BossManager);
+            SER.SpawnBoss = false;
+            SER.canspawn = false;
+            
+        }
+	}
+
+}
---
+++ Gam2002-ArcadeShooter/Gam2004-ArcadeShooter/Assets/Scripts/GoToStartScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GoToStartScreen : MonoBehaviour {
+    public GameObject Ifactive;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Ifactive.activeSelf)
+        {
+              SceneManager.LoadScene(0);
+            
+        }
+	}
+}
