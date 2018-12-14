6b0f626f1dc41f11f446dc40d8f302313774a5b5
Unknown <wmhonca@gmail.com>
2018-12-14 12:45:35 +0000

    Update/gamefilelocation
    updated

---
+++ Gam2002-ArcadeShooter/Gam2004-ArcadeShooter/Assets/Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+    public Rigidbody2D player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("a"))
+        {
+            player.AddForce(Vector2.left * 10);
+
+        }
+        if (Input.GetKey("d"))
+        {
+            player.AddForce(-Vector2.left * 10);
+
+        }
+    }
+}
---
+++ Gam2002-ArcadeShooter/Gam2004-ArcadeShooter/Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSystem : MonoBehaviour {
+    public Text ScoreBoard;
+    public Text HighScore;
+
+    public SpawnEnemiesatrandom SER;
+    public int Points = 0;
+    int bossspawnpoints;
+    public int Highpoints;
+    public int previouspoints;
+    public int nextstage;
+    bool runonce;
+    // Use this for initialization
+    void Start()
+    {
+        bossspawnpoints = 50;
+        ScoreBoard = GameObject.FindGameObjectWithTag("Score").GetComponent<Text>(); 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        previouspoints = Points;
+
+        ScoreBoard.text = "Score: " + Points.ToString();
+        HighScore.text = "High Score: " + Highpoints.ToString();
+        if (Points > nextstage)
+        {
+            NextStage();
+        }
+        if (Points >= Highpoints)
+        {
+            Highpoints = Points;
+        }
+        if (Points >= bossspawnpoints)
+        {
+            runonce = true;
+            
+          
+        }
+        if (runonce)
+        {
+            SpawnBoss();
+        }
+
+    }
+    void SpawnBoss()
+    {
+        SER.SpawnBoss = true;
+        SER.canspawn = true;        
+        bossspawnpoints += 50;
+        runonce = false;
+    }
+    void NextStage()
+    {
+        nextstage = previouspoints + 20;
+        SER.Amountallowedtospawn++;
+
+    }
+  
+}
