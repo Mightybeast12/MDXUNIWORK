275ee7a2d680db6d9c97a627f5e59acd932a85eb
Unknown <wmhonca@gmail.com>
2018-12-14 12:35:47 +0000

    12-14

---
+++ MDXUNIWORK/Gam2002-ArcadeShooter/Gam2004-ArcadeShooter/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HealthManager : MonoBehaviour {
+
+    public GameObject Heart1;
+    public GameObject Heart2;
+    public GameObject Heart3;
+    public int amountoflives = 3;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (amountoflives >= 3)
+        {
+            Heart3.SetActive(true);
+            Heart2.SetActive(true);
+            Heart1.SetActive(true);
+        }
+       
+        if (amountoflives == 2)
+        {
+            Heart3.SetActive(false);
+            Heart2.SetActive(true);
+            Heart1.SetActive(true);
+
+        }
+        if (amountoflives == 1)
+        {
+            Heart3.SetActive(false);
+            Heart2.SetActive(false);
+            Heart1.SetActive(true);
+        }
+        if (amountoflives <= 0)
+        {
+            Heart3.SetActive(false);
+            Heart2.SetActive(false);
+            Heart1.SetActive(false);
+
+            // SceneManager.LoadScene(0);
+        }
+
+
+    }
+}
---
+++ MDXUNIWORK/Gam2002-ArcadeShooter/Gam2004-ArcadeShooter/Assets/TakeDamage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeDamage : MonoBehaviour {
+    public BossHealthindicatorManager BHM;
+    public float amountofdamagetotake;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Shotgun" || other.gameObject.tag == "TwoTap"|| other.gameObject.tag == "BoomeRang")
+        {
+            BHM.Healthindicator.value -= amountofdamagetotake;
+
+        }
+    }
+}
